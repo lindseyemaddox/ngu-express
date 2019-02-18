450fbf5aa87d654d4896d26595abdf5b8db4f87a
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-02-17 22:10:24 -0600

    people working except for first child showing on load

--- public/scripts/min/main-min.js
+++ public/scripts/min/main-min.js
@@ -1,13 +1,1 @@
-$(window).scroll(function(){500<$(this).scrollTop()?$(".fade").fadeIn():$(".fade").fadeOut(),
-// Find all links within the staff container
-$(".staff a").each(function(a,e){
-// Loop through the list of links adding a click handler for each one
-$(e).on("click",function(){
-// Hide all bios to avoid showing more than one at a time
-$(".bio").hide(),
-// Show the bio with the employee data attribute that matches the link clicked
-$(".bio*[data-name="+$(this).data("name")+"]").show(),
-// Hide all name pins to avoid showing more than one at a time
-$(".staff a").removeClass("selected"),
-// Add class to the clicked link
-$(this).addClass("selected")})})});
+$(window).scroll(function(){500<$(this).scrollTop()?$(".fade").fadeIn():$(".fade").fadeOut(),$(".staff a").each(function(a,e){$(e).on("click",function(){$(".bio").hide(),$(".bio*[data-name="+$(this).data("name")+"]").show(),$(".staff a").removeClass("selected"),$(this).addClass("selected")})})});
