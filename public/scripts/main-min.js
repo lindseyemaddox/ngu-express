15e9229048dc8db5d45de712c5063a604eb47cd0
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-02-19 16:08:08 -0600

    pre removal of iterative people code

--- public/scripts/main-min.js
+++
@@ -1,1 +0,0 @@
-$(window).scroll(function(){500<$(this).scrollTop()?$(".fade").fadeIn():$(".fade").fadeOut(),$(".staff a").each(function(a,e){$(e).on("click",function(){$(".bio").hide(),$(".bio*[data-name="+$(this).data-name+"]").show(),$(".staff a").removeClass("selected"),$(this).addClass("selected")})})});
