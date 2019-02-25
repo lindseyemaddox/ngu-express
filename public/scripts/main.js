c89a9e76d18fe22fc43281389cc01a1397693a13
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-02-25 03:27:27 -0600

    responsive complete sans small people

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,4 +7,26 @@ $(window).scroll(function() {
     $( ".fade" ).fadeOut();
   }
 
+  // Find all links within the staff container
+  $( ".staff a" ).each(function(_index, link) {
+
+    // Loop through the list of links adding a click handler for each one
+    $(link).on('click', function() {
+
+      // Hide all bios to avoid showing more than one at a time
+      $( ".bio" ).hide();
+
+      // Show the bio with the employee data attribute that matches the link clicked
+      $('.bio*[data-name=' + $(this).data( "name" ) + ']').show();
+
+      // Hide all name pins to avoid showing more than one at a time
+      $( ".staff a" ).removeClass( "selected" );
+
+      // Add class to the clicked link
+      $(this).addClass( "selected" );
+
+    });
+
+  });
+
 });
